cc5208404be77a220abf1773ec5e327e8b9426b2
Jairo Guilherme Zioti <97853393+JairoZioti@users.noreply.github.com>
2022-02-27 15:23:52 -0300

    Update formatacaoNumerica.js

--- exercicios/formatacaoNumerica.js
+++ exercicios/formatacaoNumerica.js
@@ -1,3 +1,7 @@
 let salario = 1627.50
 
 console.log(salario.toLocaleString('pt-br', {style: 'currency', currency : 'brl'}));
+
+console.log(salario.toLocaleString('eua', {style: 'currency', currency : 'usd'}));
+
+console.log(salario.toLocaleString('eur', {style: 'currency', currency : 'eur'}));
