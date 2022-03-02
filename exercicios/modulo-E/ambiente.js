edbafeeeef6bd1e05fe73207d696f989673a556b
Jairo Guilherme Zioti <97853393+JairoZioti@users.noreply.github.com>
2022-03-02 11:37:33 -0300

    Update ambiente.js

--- exercicios/modulo-E/ambiente.js
+++ exercicios/modulo-E/ambiente.js
@@ -1,5 +1,49 @@
+/* Repetições em Javascript */
+
 let c = 1
-while (c < 6) {
+do {
     console.log(`Passo ${c}`)
     c++
+} while (c <= 6)
+
+/*
+##### Testa primeiro, executa depois
+
+let c = 1                      |
+while (c <= 6) {               | ===> esse é o método mais usado pelos programadores.
+    console.log(`Passo ${c}`)  |
+    c++                        |
 }
+
+
+##################################################
+
+
+##### Executa primeiro, testa depois
+
+let c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+##################################################
+
+
+##### Executa todos comandos #####
+
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+
+
+##################################################
+
+
+Todos os comandos executaram as mesmas funções, escrever no console 6 vezes.
+
+*/
