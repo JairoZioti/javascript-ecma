2c8ca97c120fa06b149eb433677930f87df979bc
Jairo Guilherme Zioti <97853393+JairoZioti@users.noreply.github.com>
2022-03-02 12:39:13 -0300

    ...

---
+++ exercicios/modulo-E/exp02.js
@@ -0,0 +1,15 @@
+/* Repetições com Variável de Controle em Javascript */
+
+/*
+
+let c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+*/
+
+for (let c = 1; c <= 6; c++) {
+    console.log(c);
+}
