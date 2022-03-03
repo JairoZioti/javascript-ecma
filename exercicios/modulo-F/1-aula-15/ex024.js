afd3b83b41c14896e3dbb734139bce1109c43206
Jairo Guilherme Zioti <97853393+JairoZioti@users.noreply.github.com>
2022-03-03 10:20:16 -0300

    Update ex024.js

--- exercicios/modulo-F/1-aula-15/ex024.js
+++ exercicios/modulo-F/1-aula-15/ex024.js
@@ -1,5 +1,10 @@
 let valores = [8, 1, 7, 4, 2, 9]
 
+console.log(`A 'variável valores' agora é uma  'array vector', pois ela tem um total de ${valores.length} elementos, e cada elemento tem 1 conteúdo`);
+
+console.log('');
+
 for (pos in valores) {
+    valores.sort()
     console.log(`A posição de índice [${pos}] tem o valor ${valores[pos]}`);
 }
